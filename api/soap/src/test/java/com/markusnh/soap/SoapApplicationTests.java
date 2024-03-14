2cacf2143171d3372a0a09b7e2c2e65b5554d765
Markus Newton Hedelin <mhedelin@kth.se>
2024-03-14 09:30:37 +0100

    Add SOAP XML schema for SOAP service

---
+++ api/soap/src/test/java/com/markusnh/soap/SoapApplicationTests.java
@@ -0,0 +1,11 @@
+package com.markusnh.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
